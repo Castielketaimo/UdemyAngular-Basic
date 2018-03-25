e8a1d99e44a6a8a871eca47407bc7a3a7a111422
Castiel <castiel.andrew@gmail.com>
2018-03-24 20:21:05 -0700

    updated styles by using inline style and inline template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,8 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
+  //styleUrls: ['./servers.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 
